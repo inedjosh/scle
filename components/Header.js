ebfe7a3b43c496d1c03d96c3d1a6de3e6e98c13d
inedjosh <inedujoshua@gmail.com>
2022-04-16 00:20:36 +0100

    work done

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -7,9 +8,47 @@ import background from '../images/background.png'
 import lady from './../images/lady.png'
 import course from './../images/course.png'
 import community from './../images/community.png'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+// import { GrClose } from 'react-icons/gr';
+import { motion } from "framer-motion";
 
 function Header() {
 
+    const [mobileNav, setMobileNav] = useState(false);
+
+  const handleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
+  const MobileShown = (
+    <motion.div
+      className={styles.mobileShown}
+      animate={mobileNav ? {   y: 0 }: {y:-1000}}
+      initial={{y: -1000 }}
+      transition={{
+        delay: 0.1,
+        duration: 0.2,
+      }}
+    >
+    <div className={styles.topNavDiv}>
+      <h2>SCLE</h2>
+    </div>
+     <div className={styles.topNavLinks}>
+     <Link href='/' className={styles.mobileLink} >
+        <a className={styles.mobileLink}> HOME</a>
+      </Link>
+        <Link href='/about' className={styles.mobileLink} >
+        <a className={styles.mobileLink}> ABOUT</a>
+      </Link>
+      <Link href='/programmes' className={styles.mobileLink}  >
+      <a className={styles.mobileLink}>PROGRAMMES</a>
+      </Link>
+      <Link href='/faculty' className={styles.mobileLink}  >
+   <a className={styles.mobileLink}>  FACULTIES</a>
+      </Link>
+     </div>
+    </motion.div>
+  );
   
 
   return (
@@ -40,10 +79,17 @@ function Header() {
       <nav className={styles.navButton}>
         APPLY NOW
       </nav>
-      <nav className={styles.menuButton}>
-            menu
+      <nav className={styles.menuButton} onClick={handleMobileNav}>
+            
+            {!mobileNav ? (
+           <AiOutlineMenu className={styles.menu}/>
+          ) : (
+            <AiOutlineClose className={styles.menu}/>
+          )}
       </nav>
-      
+      {
+        mobileNav && MobileShown
+      }
      </header>
   )
 }
--- components/HeaderWhite.js
+++ components/HeaderWhite.js
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -7,10 +8,46 @@ import background from '../images/background.png'
 import lady from './../images/lady.png'
 import course from './../images/course.png'
 import community from './../images/community.png'
+import { motion } from "framer-motion";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 function Header() {
 
-  
+   const [mobileNav, setMobileNav] = useState(false);
+
+  const handleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
+  const MobileShown = (
+    <motion.div
+      className={styles.mobileShown}
+      animate={mobileNav ? {   y: 0 }: {y:-1000}}
+      initial={{y: -1000 }}
+      transition={{
+        delay: 0.1,
+        duration: 0.2,
+      }}
+    >
+    <div className={styles.topNavDiv}>
+      <h2>SCLE</h2>
+    </div>
+     <div className={styles.topNavLinks}>
+        <Link href='/' className={styles.mobileLink} >
+        <a className={styles.mobileLink}> HOME</a>
+      </Link>
+        <Link href='/about' className={styles.mobileLink} >
+        <a className={styles.mobileLink}> ABOUT</a>
+      </Link>
+      <Link href='/programmes' className={styles.mobileLink}  >
+      <a className={styles.mobileLink}>PROGRAMMES</a>
+      </Link>
+      <Link href='/faculty' className={styles.mobileLink}  >
+   <a className={styles.mobileLink}>  FACULTIES</a>
+      </Link>
+     </div>
+    </motion.div>
+  );
 
   return (
      <header className={styles.whiteBck }>
@@ -37,7 +74,17 @@ function Header() {
 <Link href='./../faculty' className={styles.linkBlue}>FACULTIES</Link>
             </li>
           </ul>
-     
+      <nav className={styles.menuButton} onClick={handleMobileNav}>
+            
+            {!mobileNav ? (
+           <AiOutlineMenu className={styles.menuWhite}/>
+          ) : (
+            <AiOutlineClose className={styles.menuWhite}/>
+          )}
+      </nav>
+      {
+        mobileNav && MobileShown
+      }
       
      </header>
   )
