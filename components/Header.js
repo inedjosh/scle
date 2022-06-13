6da7c52aed7088dad7dd3be40b19f4146a771ef4
inedu joshua <inedujoshua@gmail.com>
2022-06-13 16:00:06 +0100

    making updates

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,19 @@
-import React, {useState} from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import logo from './../images/MAIN-LOGO.png'
-import Link from 'next/link'
-import background from '../images/background.png'
-import lady from './../images/lady.png'
-import course from './../images/course.png'
-import community from './../images/community.png'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import logo from "./../images/logo-new.svg";
+import Link from "next/link";
+import background from "../images/background.png";
+import lady from "./../images/lady.png";
+import course from "./../images/course.png";
+import community from "./../images/community.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 // import { GrClose } from 'react-icons/gr';
 import { motion } from "framer-motion";
 
 function Header() {
-
-    const [mobileNav, setMobileNav] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
 
   const handleMobileNav = () => {
     setMobileNav(!mobileNav);
@@ -23,77 +22,71 @@ function Header() {
   const MobileShown = (
     <motion.div
       className={styles.mobileShown}
-      animate={mobileNav ? {   y: 0 }: {y:-1000}}
-      initial={{y: -1000 }}
+      animate={mobileNav ? { y: 0 } : { y: -1000 }}
+      initial={{ y: -1000 }}
       transition={{
         delay: 0.1,
         duration: 0.4,
       }}
     >
-    <div className={styles.topNavDiv}>
-      <h2>SCLE</h2>
-    </div>
-     <div className={styles.topNavLinks}>
-     <Link href='/' className={styles.mobileLink} >
-        <a className={styles.mobileLink}> HOME</a>
-      </Link>
-        <Link href='/about' className={styles.mobileLink} >
-        <a className={styles.mobileLink}> ABOUT</a>
-      </Link>
-      <Link href='/programmes' className={styles.mobileLink}  >
-      <a className={styles.mobileLink}>PROGRAMMES</a>
-      </Link>
-      <Link href='/faculty' className={styles.mobileLink}  >
-   <a className={styles.mobileLink}>  FACULTIES</a>
-      </Link>
-     </div>
+      <div className={styles.topNavDiv}>
+        <h2>SCLE</h2>
+      </div>
+      <div className={styles.topNavLinks}>
+        <Link href="/" className={styles.mobileLink}>
+          <a className={styles.mobileLink}> HOME</a>
+        </Link>
+        <Link href="/about" className={styles.mobileLink}>
+          <a className={styles.mobileLink}> ABOUT</a>
+        </Link>
+        <Link href="/programmes" className={styles.mobileLink}>
+          <a className={styles.mobileLink}>PROGRAMMES</a>
+        </Link>
+        <Link href="/faculty" className={styles.mobileLink}>
+          <a className={styles.mobileLink}> FACULTIES</a>
+        </Link>
+      </div>
     </motion.div>
   );
-  
 
   return (
-     <header className={styles.header}>
-  <Link href='/' passHref>
-  <a className={styles.logoDiv}>
-       <Image
-        src={logo}
-        className={styles.logo}
-        alt='image'
-        />
+    <header className={styles.header}>
+      <Link href="/" passHref>
+        <a className={styles.logoDiv}>
+          <Image src={logo} className={styles.logo} alt="image" />
         </a>
-        </Link>
-          <ul className={styles.nav}>
-            <li className={styles.navLink}>
-<Link href='./../about' className={styles.link}>ABOUT US</Link>
-            </li>
-            <li className={styles.navLink}>
-<Link href='./../programmes' className={styles.link}>PROGRAMMES</Link>
-            </li>
-            {/* <li className={styles.navLink}>
+      </Link>
+      <ul className={styles.nav}>
+        <li className={styles.navLink}>
+          <Link href="./../about" className={styles.link}>
+            ABOUT US
+          </Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link href="./../programmes" className={styles.link}>
+            PROGRAMMES
+          </Link>
+        </li>
+        {/* <li className={styles.navLink}>
 <Link href='/faq' className={styles.link}>FAQ</Link>
             </li> */}
-            <li className={styles.navLink}>
-<Link href='./../faculty' className={styles.link}>FACULTIES</Link>
-            </li>
-          </ul>
-      <nav className={styles.navButton}>
-        APPLY NOW
-      </nav>
+        <li className={styles.navLink}>
+          <Link href="./../faculty" className={styles.link}>
+            FACULTIES
+          </Link>
+        </li>
+      </ul>
+      <nav className={styles.navButton}>APPLY NOW</nav>
       <nav className={styles.menuButton} onClick={handleMobileNav}>
-            
-            {!mobileNav ? (
-           <AiOutlineMenu className={styles.menu}/>
-          ) : (
-            <AiOutlineClose className={styles.menu}/>
-          )}
+        {!mobileNav ? (
+          <AiOutlineMenu className={styles.menu} />
+        ) : (
+          <AiOutlineClose className={styles.menu} />
+        )}
       </nav>
-      {
-        mobileNav && MobileShown
-      }
-     </header>
-  )
+      {mobileNav && MobileShown}
+    </header>
+  );
 }
 
-export default Header
-
-
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,132 +1,141 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import logo from './../images/MAIN-LOGO.png'
-import lady from './../images/lady.png'
-import background from './../images/background.png'
-import Link from 'next/link'
-import course from './../images/guy-home.png'
-import community from './../images/community.png'
-import Header from './../components/Header'
-import backgroundTestimony from './../images/backgroundTestimony.png'
-import image1 from './../images/image1.png'
-import image2 from './../images/image2.png'
-import image3 from './../images/image3.png'
-import contact from './../images/contact-home.png'
-import alumni from './../images/alumni.png'
-import Footer from './../components/Footer'
-import { HiOutlineAcademicCap } from 'react-icons/hi';
-import { AiOutlineCalendar } from 'react-icons/ai';
-import { IoIosPeople } from 'react-icons/io';
-import homeImage1 from './../images/home-img1.png'
-import homeImage2 from './../images/home-img2.png'
-import homeImage3 from './../images/homeimg-3.png'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import logo from "./../images/MAIN-LOGO.png";
+import lady from "./../images/lady.png";
+import background from "./../images/new-hero.jpg";
+import Link from "next/link";
+import course from "./../images/guy-home.png";
+import community from "./../images/community.png";
+import Header from "./../components/Header";
+import backgroundTestimony from "./../images/backgroundTestimony.png";
+import image1 from "./../images/image1.png";
+import image2 from "./../images/image2.png";
+import image3 from "./../images/image3.png";
+import contact from "./../images/contact-home.png";
+import alumni from "./../images/alumni.png";
+import Footer from "./../components/Footer";
+import { HiOutlineAcademicCap } from "react-icons/hi";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
+import homeImage1 from "./../images/home-img1.png";
+import homeImage2 from "./../images/home-img2.png";
+import homeImage3 from "./../images/homeimg-3.png";
 
 export default function Home() {
   return (
-
     <div className={styles.container}>
-  
-   <Header />
-     <div className={styles.hero} >
-
-      <div className={styles.containerHero}>
-        <Image src={background}  className={styles.background} />
+      <Header />
+      <div className={styles.hero}>
+        <div className={styles.containerHero}>
+          <Image src={background} className={styles.background} />
+        </div>
+        <div className={styles.overlay}></div>
+        <div className={styles.heroDescription}>
+          <div className={styles.heroText}>
+            To spread prosperity
+            <br /> and peace for everyone <br /> every where in the{" "}
+            <span className={styles.heroTextBlue}>world</span>
+          </div>
+          <div className={styles.heroTextSub}>
+            <div className={styles.redBar}></div>
+            <p className={styles.heroParagraph}>
+              Leveraging the wisdom entrenched in the Word of God, effective
+              leaders will be groomed as beacons of light in industry and
+              governance around the world.
+            </p>
+          </div>
+        </div>
       </div>
-     <div className={styles.overlay}></div>
-   <div className={styles.heroDescription}>
-      <div className={styles.heroText}>
-  To spread  prosperity<br/> and peace 
-   for  everyone  <br/>  every where in the  <span className={styles.heroTextBlue}>world</span>
-    </div>
-    <div className={styles.heroTextSub}>
-      <div className={styles.redBar}></div>
-      <p className={styles.heroParagraph}> 
-      Leveraging the wisdom entrenched in the Word of God effective leaders will be groomed as beacons of light in industry and governance around the world.  
 
- </p>
-    </div>
-   </div>
-     </div>
-    
- 
-     {/* <div className={styles.subHero}>
+      {/* <div className={styles.subHero}>
        <h4 className={styles.subHeroHeadingText}>charles darwin</h4>
        <p className={styles.subHeroParagraph}>
          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi neque pulvinar erat amet porttitor amet, blandit vulputate. Sed adipiscing dignissim placerat 
        </p>
        </div> */}
-       <div className={styles.whoWeAre}>
-       <h1 className={styles.mainHeading}>Who we are </h1> 
+      <div className={styles.whoWeAre}>
+        <h1 className={styles.mainHeading}>Who we are </h1>
 
-       <div className={styles.flexDiv}>
-
-      <div className={styles.textDiv}>
-       <h2 className={styles.subHeading}>Leading leadership institute in nigeria and large</h2>
-       <p className={styles.subHeadingText}>School of Christian Leadership and Entrepreneurship (SCLE) is a capacity building intervention that promotes excellent leadership and business management practices that are sourced from biblical principles and practices. SCLE is a faith-based institute that promotes Christian values in public and private life as a form of evangelism for prosperity and social peace and their sustainability. SCLE builds the capacity of public and private leaders and managers to identity critical issues, processes and values that promotes prosperity, harmonious interdependence and social justice and establish consensus and commitment on institutions that guarantee sustainable prosperity and freedom. To achieve sustainable prosperity and just social order, SCLE focuses on imparting technical skills required to drive innovative business and innovative practices that can create prosperity and turn ideas from dreams into enterprise   </p>
-       {/* <div className={styles.btnDiv}>Learn more</div> */}
-       </div>
-      <div className={styles.whoWeAreImageDiv}>
-           <Image
-        src={lady}
-        className={styles.divImageMain}
-          alt='image'
-        />
-        <div className={styles.overlapImages}>
-           <Image
-        src={homeImage1}
-        className={styles.divImage}
-          alt='image'
-        />
-           <Image
-        src={homeImage2}
-        className={styles.divImage}
-          alt='image'
-        />
-           <Image
-        src={homeImage3}
-        className={styles.divImage}
-          alt='image'
-        />
-</div>
+        <div className={styles.flexDiv}>
+          <div className={styles.textDiv}>
+            <h2 className={styles.subHeading}>
+              Leading leadership institute in nigeria and large
+            </h2>
+            <p className={styles.subHeadingText}>
+              School of Christian Leadership and Entrepreneurship (SCLE) is a
+              capacity building intervention that promotes excellent leadership
+              and business management practices that are sourced from biblical
+              principles and practices. SCLE is a faith-based institute that
+              promotes Christian values in public and private life as a form of
+              evangelism for prosperity and social peace and their
+              sustainability. SCLE builds the capacity of public and private
+              leaders and managers to identity critical issues, processes and
+              values that promotes prosperity, harmonious interdependence and
+              social justice and establish consensus and commitment on
+              institutions that guarantee sustainable prosperity and freedom. To
+              achieve sustainable prosperity and just social order, SCLE focuses
+              on imparting technical skills required to drive innovative
+              business and innovative practices that can create prosperity and
+              turn ideas from dreams into enterprise{" "}
+            </p>
+            {/* <div className={styles.btnDiv}>Learn more</div> */}
+          </div>
+          <div className={styles.whoWeAreImageDiv}>
+            <Image src={lady} className={styles.divImageMain} alt="image" />
+            <div className={styles.overlapImages}>
+              <Image src={homeImage1} className={styles.divImage} alt="image" />
+              <Image src={homeImage2} className={styles.divImage} alt="image" />
+              <Image src={homeImage3} className={styles.divImage} alt="image" />
+            </div>
+          </div>
+        </div>
       </div>
-    
-</div>
-       </div>
-       <div className={styles.programme}>
-       <div className={styles.backgroundHeading}>
-         <h3 className={styles.headingText}>Our Mission</h3>
-         {/* <div className={styles.headingBtn}>
+      <div className={styles.programme}>
+        <div className={styles.backgroundHeading}>
+          <h3 className={styles.headingText}>Our Mission</h3>
+          {/* <div className={styles.headingBtn}>
            Learn more
          </div> */}
-       </div>
-       <div className={styles.sectionImageDiv}>
-         <div className={styles.hide}>
-            <Image
-        src={course}
-        className={styles.sectionImage}
-          alt='image'
-        />
-         </div>
-        <div className={styles.overlap}>
-         {/* <div>
+        </div>
+        <div className={styles.sectionImageDiv}>
+          <div className={styles.hide}>
+            <Image src={course} className={styles.sectionImage} alt="image" />
+          </div>
+          <div className={styles.overlap}>
+            {/* <div>
             <h2 className={styles.overlapHeading}>certificate program in christian leadership</h2>
          </div> */}
-         <div className={styles.overlapTextDiv}>
-           <p className={styles.overlapText}>Our mission is to build a prosperous and peaceful environment for everyone by equipping global leaders with ethical and technical capabilities through an innovative and value-based leadership and management education. We leverage on the wisdom revealed in the Word of God to groom effective leaders to become beacons of light in industry and governance around the world.   </p>
-         </div>
-         <div className={styles.overlapList}>
-           <p className={styles.listOverlap}><AiOutlineCalendar className={styles.whiteColor}/> 3 Months</p>
-           <p className={styles.listOverlap}><HiOutlineAcademicCap className={styles.whiteColor} /> 6 Faculties</p>
-           <p className={styles.listOverlap}><IoIosPeople className={styles.whiteColor}/>178 Students</p>
-         </div>
-         <div>
-           <button className={styles.overlapButton}>join programme</button>
-         </div>
+            <div className={styles.overlapTextDiv}>
+              <p className={styles.overlapText}>
+                Our mission is to build a prosperous and peaceful environment
+                for everyone by equipping global leaders with ethical and
+                technical capabilities through an innovative and value-based
+                leadership and management education. We leverage on the wisdom
+                revealed in the Word of God to groom effective leaders to become
+                beacons of light in industry and governance around the world.{" "}
+              </p>
+            </div>
+            <div className={styles.overlapList}>
+              <p className={styles.listOverlap}>
+                <AiOutlineCalendar className={styles.whiteColor} /> 3 Months
+              </p>
+              <p className={styles.listOverlap}>
+                <HiOutlineAcademicCap className={styles.whiteColor} /> 6
+                Faculties
+              </p>
+              <p className={styles.listOverlap}>
+                <IoIosPeople className={styles.whiteColor} />
+                178 Students
+              </p>
+            </div>
+            <div>
+              <button className={styles.overlapButton}>join programme</button>
+            </div>
+          </div>
         </div>
-       </div>
-       </div>
-       {/* <div className={styles.programme}>
+      </div>
+      {/* <div className={styles.programme}>
        <div className={styles.backgroundHeading}>
          <h3 className={styles.headingText}>Community updates</h3>
          <div className={styles.headingBtn}>
@@ -141,76 +150,77 @@ export default function Home() {
         />
        </div>
        </div> */}
-       <div className={styles.testimony}>
-         <Image
-        src={backgroundTestimony}
-        className={styles.testimonyImage}
-          alt='image'
+      <div className={styles.testimony}>
+        <Image
+          src={backgroundTestimony}
+          className={styles.testimonyImage}
+          alt="image"
         />
         <div className={styles.testimonyBox}>
-<h3 className={styles.headingText}>Testimonials</h3>
+          <h3 className={styles.headingText}>Testimonials</h3>
 
-  <p className={styles.testimonyText}>
-  SCLE is the best place to be, indeed joining the program has changed my life alot!</p>
-   <div className={styles.testimonyImageDiv}>
-     <Image
-        src={image1}
-        className={styles.testimonyPerson} 
-         alt='image'
-
-        />
-   </div>
-</div>
- 
-       </div>
-       <div className={styles.contact}>
-       <div className={styles.cantactImageDiv}>
-        <div className={styles.hide}>
-             <Image
-        src={contact}
-        className={styles.contactImage}
-        // height={900}
-       alt='image'
-        />
+          <p className={styles.testimonyText}>
+            SCLE is the best place to be, indeed joining the program has changed
+            my life alot!
+          </p>
+          <div className={styles.testimonyImageDiv}>
+            <Image
+              src={image1}
+              className={styles.testimonyPerson}
+              alt="image"
+            />
+          </div>
         </div>
+      </div>
+      <div className={styles.contact}>
+        <div className={styles.cantactImageDiv}>
+          <div className={styles.hide}>
+            <Image
+              src={contact}
+              className={styles.contactImage}
+              // height={900}
+              alt="image"
+            />
+          </div>
         </div>
         <div className={styles.form}>
-        <h2 className={styles.bigHeading}>Stay updated</h2>
-        <p className={styles.formText}>stay updated on news, events, entrepreneurs, research and more</p>
-       <div className={styles.formDiv}>
-         <label className={styles.label}>Email Address <span className={styles.redColor}>*</span></label>
-          <input className={styles.input} type='text' />
-       </div>
-      
-       <div className={styles.formBtnDiv}>
-       <button className={styles.formBtn}>Submit</button>
-       </div>
-        </div>
-       </div>
-       <div className={styles.alumni}>
-         <h2 className={styles.alumniHeading}> Partner with our alumni</h2>
-         <p className={styles.alumniTextTop}>
-        Leveraging the wisdom entrenched in the Word of God effective leaders will be groomed as beacons of light in industry and governance around the world.  
-        </p>
-        <div className={styles.partnerImage}>
-           <Image src={alumni}
-          alt='image'
-          className={styles.alumniImage}
-         />
-        </div>
-       
-<p className={styles.alumniTextBottom}>
-To build a world of prosperity and peace for every one by equipping global leaders with ethical and technical capabilities through an innovative and value-based leadership and management education
-
-</p>
-<div className={styles.alumniButtonDiv}>
-  <button className={styles.alumniButton}>
-  Connect Now
-</button>
-</div>
-       </div>
-       <Footer />
+          <h2 className={styles.bigHeading}>Stay updated</h2>
+          <p className={styles.formText}>
+            stay updated on news, events, entrepreneurs, research and more
+          </p>
+          <div className={styles.formDiv}>
+            <label className={styles.label}>
+              Email Address <span className={styles.redColor}>*</span>
+            </label>
+            <input className={styles.input} type="text" />
           </div>
 
-  )
+          <div className={styles.formBtnDiv}>
+            <button className={styles.formBtn}>Submit</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.alumni}>
+        <h2 className={styles.alumniHeading}> Partner with our alumni</h2>
+        <p className={styles.alumniTextTop}>
+          Leveraging the wisdom entrenched in the Word of God effective leaders
+          will be groomed as beacons of light in industry and governance around
+          the world.
+        </p>
+        <div className={styles.partnerImage}>
+          <Image src={alumni} alt="image" className={styles.alumniImage} />
+        </div>
+
+        <p className={styles.alumniTextBottom}>
+          To build a world of prosperity and peace for every one by equipping
+          global leaders with ethical and technical capabilities through an
+          innovative and value-based leadership and management education
+        </p>
+        <div className={styles.alumniButtonDiv}>
+          <button className={styles.alumniButton}>Connect Now</button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
